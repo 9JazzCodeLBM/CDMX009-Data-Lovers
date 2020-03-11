1abfb3e14b9f68a8eb5d910f64dcd4edadd186a6
paotm <ipao.tm2202@gmail.com>
2020-03-11 15:03:51 -0600

    agrupacion de filtros

--- src/data.js
+++ src/data.js
@@ -1,34 +1,4 @@
 
 import data from './data/pokemon/pokemon.js';
-let list = data.pokemon
 
-list.map(p => console.log(p.name));
-list.map(p => console.log(p.type));
-//let nombre = list[151].name;
-
-// esta es una función de ejemplo
-//var Contenido = document.querySelector(#tipos)
-//export const example = () => {
-//return 'example';
-//}
-//let nombre = list[151].name;
-//list.map(p=>console.log(p.name));
-//let filtrada = list.filter(p=>p.name==="Pikachu"); // filter devuelve una lista filtrada
-let electrics = list.filter(p => p.type.includes("Electric"));
-console.log(electrics);
-// Filtra los pokemons por weaknesses: Fire y muestralos en consola
-let weAk = list.filter(p => p.weaknesses.includes("Fairy"));
-console.log(weAk);
-//let pokes = document.querySelector('#pokes');
-
-//weAk.map(pok=>{
-  //let p = document.createElement('p');
-  //p.classList.add("card");
-  //p.innerHTML = `
-//<img src=${pok.img} />
-//<p>${pok.name}</p>
-//`;
-//  pokes.appendChild(p);
-//});
-
-// map, filter, forEach, event: change (select)
+export const getCharacterbyType = (type) => data.pokemon.filter(p=>p.type.includes(type))
