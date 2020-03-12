c8a077689e24e7a649877e3c1364ebd3602ff581
paotm <ipao.tm2202@gmail.com>
2020-03-12 16:18:34 -0600

    muestra imagenes

--- src/main.js
+++ src/main.js
@@ -1,68 +1,25 @@
 import {getCharacterbyType} from './data.js';
+import data from './data/pokemon/pokemon.js';
 
-
+// let list= data.pokemon;
 //Nodo
 const types = document.querySelectorAll(".type");
+const poke = document.querySelector('#pokes');
 
 //Listener
-types.forEach(b=>b.addEventListener("click", e=>{console.log(getCharacterbyType(e.target.id))
+types.forEach(b=>b.addEventListener("click", e=>{createCard(getCharacterbyType(e.target.id))
 }
 ))
 
 
- //let list = data.pokemon
+ function createCard(array) {
+   poke.innerHTML = '';
+  for (let pokemon of array) {
+    console.log(array)
+  poke.innerHTML += `
 
-// list.map(p => console.log(p.name));
-// list.map(p => console.log(p.type));
-//
-//
-// let electric= list.filter(p => p.type.includes("Electric"));
-// console.log(electric);
-// let grass = list.filter(p => p.type.includes("Grass"));
-// console.log(grass);
-// let poison = list.filter(p => p.type.includes("Poison"));
-// console.log(poison);
-// let fire = list.filter(p => p.type.includes("Fire"));
-// console.log(fire);
-// let flying = list.filter(p => p.type.includes("Flying"));
-// console.log(flying);
- let water = list.filter(p => p.type.includes("Water"));
-// console.log(water);
-// let bug = list.filter(p => p.type.includes("Bug"));
-// console.log(bug);
-// let normal = list.filter(p => p.type.includes("Normal"));
-// console.log(normal);
-// let ground = list.filter(p => p.type.includes("Ground"));
-// console.log(ground);
-// let psychic = list.filter(p => p.type.includes("Psychic"));
-// console.log(psychic);
-// let rock = list.filter(p => p.type.includes("Rock"));
-// console.log(rock);
-// let ice = list.filter(p => p.type.includes("Ice"));
-// console.log(ice);
-// let ghost = list.filter(p => p.type.includes("Ghost"));
-// console.log(ghost);
-// let fighting = list.filter(p => p.type.includes("Fighting"));
-// console.log(fighting);
-// let dragon = list.filter(p => p.type.includes("Dragon"));
-// console.log(dragon);
-//
-// //export
-// const getCharacterbyType = (type) => data.pokemon.filter(p=>p.type.includes(type))
-// console.log(getCharacterbyType());
-
-// // Filtra los pokemons por weaknesses: Fire y muestralos en consola
-// let weAk = list.filter(p=>p.weaknesses.includes("Fairy"));
-// console.log(weAk);
-let pokes = document.querySelector('#pokes');
-//
-//   1
-water.map(pok=>{
-  let p = document.createElement('p');
- p.classList.add("card");
-   p.innerHTML = `
- <img class="imag" src=${pok.img} />
- <p>${pok.name}</p>
- `;
- pokes.appendChild(p);
- });
+  <img class="imag" src=${pokemon.img} />
+  <p>${pokemon.name}</p>
+  `
+ }
+};
