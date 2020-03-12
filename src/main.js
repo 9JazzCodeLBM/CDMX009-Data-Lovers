51a230f6709c141dffe5cf17863d9479008222fa
paotm <ipao.tm2202@gmail.com>
2020-03-12 11:35:11 -0600

    funciones

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-import data from './data/pokemon/pokemon.js';
-let list = data.pokemon
-
-list.map(p => console.log(p.name));
-list.map(p => console.log(p.type));
+import {getCharacterbyType} from './data.js';
 
 
 //Nodo
@@ -13,7 +9,8 @@ types.forEach(b=>b.addEventListener("click", e=>{console.log(getCharacterbyType(
 }
 ))
 
- let list = data.pokemon
+
+ //let list = data.pokemon
 
 // list.map(p => console.log(p.name));
 // list.map(p => console.log(p.type));
@@ -29,7 +26,7 @@ types.forEach(b=>b.addEventListener("click", e=>{console.log(getCharacterbyType(
 // console.log(fire);
 // let flying = list.filter(p => p.type.includes("Flying"));
 // console.log(flying);
-// let water = list.filter(p => p.type.includes("Water"));
+ let water = list.filter(p => p.type.includes("Water"));
 // console.log(water);
 // let bug = list.filter(p => p.type.includes("Bug"));
 // console.log(bug);
@@ -57,15 +54,15 @@ types.forEach(b=>b.addEventListener("click", e=>{console.log(getCharacterbyType(
 // // Filtra los pokemons por weaknesses: Fire y muestralos en consola
 // let weAk = list.filter(p=>p.weaknesses.includes("Fairy"));
 // console.log(weAk);
-// let pokes = document.querySelector('#pokes');
+let pokes = document.querySelector('#pokes');
 //
 //   1
-// water.map(pok=>{
-//   let p = document.createElement('p');
-//   p.classList.add("card");
-//   p.innerHTML = `
-// <img class="imag" src=${pok.img} />
-// <p>${pok.name}</p>
-// `;
-//   pokes.appendChild(p);
-// });
+water.map(pok=>{
+  let p = document.createElement('p');
+ p.classList.add("card");
+   p.innerHTML = `
+ <img class="imag" src=${pok.img} />
+ <p>${pok.name}</p>
+ `;
+ pokes.appendChild(p);
+ });
