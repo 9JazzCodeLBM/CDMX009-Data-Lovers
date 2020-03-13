4e99bcebe21f5489ddd170243d44195f244a1e65
paotm <ipao.tm2202@gmail.com>
2020-03-13 12:26:37 -0600

    cards

--- src/main.js
+++ src/main.js
@@ -17,9 +17,17 @@ types.forEach(b=>b.addEventListener("click", e=>{createCard(getCharacterbyType(e
   for (let pokemon of array) {
     console.log(array)
   poke.innerHTML += `
-
+<div id="uno">
   <img class="imag" src=${pokemon.img} />
-  <p>${pokemon.name}</p>
+    <p>${pokemon.name}</p>
+    </div>
+    <div id="dos">
+  <p>Numero: ${pokemon.num}</p>
+  <p>Tipo de dulce ${pokemon.candy}</p>
+  <p>Dulces para evolucionar ${pokemon.candy_count}</p>
+
+
+  </div>
   `
  }
 };
