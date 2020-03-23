2bc84d918c9f9a1772aa37b642608a102b48b84d
9JazzCodeLBM <laura.bailon09@gmail.com>
2020-03-23 13:12:04 -0600

    cambios slide

--- src/main.js
+++ src/main.js
@@ -1,96 +1,33 @@
-<<<<<<< HEAD
+import {getCharacterbyType} from './data.js';
 import data from './data/pokemon/pokemon.js';
-let list = data.pokemon
 
-list.map(p => console.log(p.name));
-list.map(p => console.log(p.type));
-=======
+// let list= data.pokemon;
+//Nodo
+const types = document.querySelectorAll(".type");
+const poke = document.querySelector('#pokes');
 
-import data from './data/pokemon/pokemon.js';
-let list = data.pokemon
->>>>>>> 23fbad835359c27daecd1ff26a0d0220186682a1
+//Listener
+types.forEach(b=>b.addEventListener("click", e=>{createCard(getCharacterbyType(e.target.id))
+}
+))
 
-list.map(p => console.log(p.name));
-list.map(p => console.log(p.type));
 
-<<<<<<< HEAD
-let electric= list.filter(p => p.type.includes("Electric"));
-console.log(electric);
-let grass = list.filter(p => p.type.includes("Grass"));
-console.log(grass);
-let poison = list.filter(p => p.type.includes("Poison"));
-console.log(poison);
-let fire = list.filter(p => p.type.includes("Fire"));
-console.log(fire);
-let flying = list.filter(p => p.type.includes("Flying"));
-console.log(flying);
-let water = list.filter(p => p.type.includes("Water"));
-console.log(water);
-let bug = list.filter(p => p.type.includes("Bug"));
-console.log(bug);
-let normal = list.filter(p => p.type.includes("Normal"));
-console.log(normal);
-let ground = list.filter(p => p.type.includes("Ground"));
-console.log(ground);
-let psychic = list.filter(p => p.type.includes("Psychic"));
-console.log(psychic);
-let rock = list.filter(p => p.type.includes("Rock"));
-console.log(rock);
-let ice = list.filter(p => p.type.includes("Ice"));
-console.log(ice);
-let ghost = list.filter(p => p.type.includes("Ghost"));
-console.log(ghost);
-let fighting = list.filter(p => p.type.includes("Fighting"));
-console.log(fighting);
-let dragon = list.filter(p => p.type.includes("Dragon"));
-console.log(dragon);
+ function createCard(array) {
+   poke.innerHTML = '';
+  for (let pokemon of array) {
+    console.log(array)
+  poke.innerHTML += `
+<div id="uno">
+  <img class="imag" src=${pokemon.img} />
+    <p>${pokemon.name}</p>
+    </div>
+    <div id="dos">
+  <p>Numero: ${pokemon.num}</p>
+  <p>Tipo de dulce ${pokemon.candy}</p>
+  <p>Dulces para evolucionar ${pokemon.candy_count}</p>
 
-=======
 
-let electric= list.filter(p => p.type.includes("Electric"));
-console.log(electric);
-let grass = list.filter(p => p.type.includes("Grass"));
-console.log(grass);
-let poison = list.filter(p => p.type.includes("Poison"));
-console.log(poison);
-let fire = list.filter(p => p.type.includes("Fire"));
-console.log(fire);
-let flying = list.filter(p => p.type.includes("Flying"));
-console.log(flying);
-let water = list.filter(p => p.type.includes("Water"));
-console.log(water);
-let bug = list.filter(p => p.type.includes("Bug"));
-console.log(bug);
-let normal = list.filter(p => p.type.includes("Normal"));
-console.log(normal);
-let ground = list.filter(p => p.type.includes("Ground"));
-console.log(ground);
-let psychic = list.filter(p => p.type.includes("Psychic"));
-console.log(psychic);
-let rock = list.filter(p => p.type.includes("Rock"));
-console.log(rock);
-let ice = list.filter(p => p.type.includes("Ice"));
-console.log(ice);
-let ghost = list.filter(p => p.type.includes("Ghost"));
-console.log(ghost);
-let fighting = list.filter(p => p.type.includes("Fighting"));
-console.log(fighting);
-let dragon = list.filter(p => p.type.includes("Dragon"));
-console.log(dragon);
-
->>>>>>> 23fbad835359c27daecd1ff26a0d0220186682a1
-
-// Filtra los pokemons por weaknesses: Fire y muestralos en consola
-let weAk = list.filter(p=>p.weaknesses.includes("Fairy"));
-console.log(weAk);
-let pokes = document.querySelector('#pokes');
-
-water.map(pok=>{
-  let p = document.createElement('p');
-  p.classList.add("card");
-  p.innerHTML = `
-<img class="imag" src=${pok.img} />
-<p>${pok.name}</p>
-`;
-  pokes.appendChild(p);
-});
+  </div>
+  `
+ }
+};
